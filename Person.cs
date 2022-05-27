b857cb4fda079e7b3141814d1f3ba52d6a156bbb
Fatima Pilabre <fpilabre8599@smail.pcd.edu>
2022-05-26 20:12:22 -0700

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClassesPractice
+{
+    public class Person
+    {
+        // Create a constructor
+        public Person() { }
+
+        // Properties
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+
+        // methods
+        public void DisplayName( )
+        {     
+            Console.WriteLine(" My Name is: {0} {1}", FirstName, LastName);
+        }
+        public void DisplayEmail()
+        {
+            Console.WriteLine("Email is: {0}", Email);
+        }
+ 
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleClassesPractice;
+
+Console.WriteLine("hello; world");
+
+
+// instance1
+Person person = new Person();
+person.FirstName = "Julie";
+person.LastName = "Waterfall";
+person.Email = "Julie@class.com";
+person.DisplayName();
+person.DisplayEmail();
+
+
+//instance2
+var person1 = new Person();
+person1.FirstName = "Claude";
+person1.LastName = "Wood";
+person1.Email = "Wood@class.com";
+person1.DisplayName();
+person1.DisplayEmail();
+
+// instance3
+var person2 = new Person();
+person2.FirstName = "Avy";
+person2.LastName = "Rid";
+person2.Email = "Avy@class,com";
+person2.DisplayName();
+person2.DisplayEmail();
+
+
+
+
+
